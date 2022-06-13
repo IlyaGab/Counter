07075e3d990be1a4a401e7c25188b59a30ffbdc5
IlyaGab <ilyagab1994@gmail.com>
2022-06-13 20:06:32 +0300

    refactor reducer

--- src/redux/counter-reducer.ts
+++ src/redux/counter-reducer.ts
@@ -1,5 +1,11 @@
 export type StatusType = 'set' | 'count' | 'error'
 
+const INC_VALUE = 'INC-VALUE'
+const RESET_VALUE = 'RESET-VALUE'
+const CHANGE_MAX_VALUE = 'CHANGE-MAX-VALUE'
+const CHANGE_MIN_VALUE = 'CHANGE-MIN-VALUE'
+const CHANGE_STATUS = 'CHANGE-STATUS'
+
 const initialState = {
     count: 0,
     minValue: 0,
@@ -11,26 +17,26 @@ type InitialStateType = typeof initialState
 
 export const counterReducer = (state: InitialStateType = initialState, action: ActionTypes): InitialStateType => {
     switch (action.type){
-        case 'INC-VALUE':
+        case INC_VALUE:
            return {...state, count: state.count + 1}
-        case 'RESET-VALUE':
+        case RESET_VALUE:
             return {...state, count: action.minValue}
-        case 'CHANGE-MAX-VALUE':
+        case CHANGE_MAX_VALUE:
             return {...state, maxValue: action.maxValue }
-        case 'CHANGE-MIN-VALUE':
+        case CHANGE_MIN_VALUE:
             return {...state, minValue: action.minValue}
-        case 'CHANGE-STATUS':
+        case CHANGE_STATUS:
             return {...state, status: action.status}
         default:
             return state
     }
 }
 
-export const incCounterValueAC = () => ({type:'INC-VALUE'}) as const;
-export const resCounterValueAC = (minValue:number) => ({type:'RESET-VALUE', minValue}) as const
-export const ChangeMaxValueAC = (maxValue:number) =>  ({type:'CHANGE-MAX-VALUE' , maxValue}) as const;
-export const ChangeMinValueAC = (minValue:number) => ({type:'CHANGE-MIN-VALUE',  minValue}) as const;
-export const ChangeStatusAC = (status:StatusType)=> ({type: 'CHANGE-STATUS', status}) as const;
+export const incCounterValueAC = () => ({type:INC_VALUE}) as const;
+export const resCounterValueAC = (minValue:number) => ({type:RESET_VALUE, minValue}) as const
+export const ChangeMaxValueAC = (maxValue:number) =>  ({type: CHANGE_MAX_VALUE , maxValue}) as const;
+export const ChangeMinValueAC = (minValue:number) => ({type:CHANGE_MIN_VALUE,  minValue}) as const;
+export const ChangeStatusAC = (status:StatusType)=> ({type: CHANGE_STATUS, status}) as const;
 
 export type IncCountActionType = ReturnType<typeof incCounterValueAC>
 export type ResCounterActionType = ReturnType<typeof resCounterValueAC>
